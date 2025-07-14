3702d4b2c2eb68cfc9f2c0100c1261dcd0c807a0
Rodrigo Lopes Velloso <135156904+velloso99@users.noreply.github.com>
2025-07-14 16:01:55 -0300

    index

---
+++ assents/js/ms.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector(".menulinks");
+let menudois = document.querySelector("#menubtn");
+
+
+menudois.onclick = () => {
+    menu.classList.toggle('active');
+}
+
+
+
+
+
