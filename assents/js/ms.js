97fa66fd90ca751cb7f058fabc9b0e8fc49caa5c
Rodrigo Lopes Velloso <135156904+velloso99@users.noreply.github.com>
2025-07-18 18:32:56 -0300

    assents

--- assents/js/ms.js
+++ assents/js/ms.js
@@ -5,8 +5,32 @@ let menudois = document.querySelector("#menubtn");
 menudois.onclick = () => {
     menu.classList.toggle('active');
 }
+/*menu do site*/
+window.onscroll = () =>{
+    let topoSite = document.querySelector("header");
+
+    topoSite.classList.toggle('sticky', window.scrollY> 100);
+}
 
 
+/*slide destaque*/
+var swiper = new Swiper(".msSlider",{
+    spaceBetween: 30,
+    centeredSlide: true,
+    loop: true,
+    autoplay: {
+        delay: 4500,
+        disbledOnInteraction: false,
+    },
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 
 
